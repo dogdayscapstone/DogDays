e29525bc5c51de5b6170ca64993b56ba87ef3c84
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-05-14 14:30:29 -0500

    added dog profile and edit dog profile page

--- src/main/java/com/codeup/dogdays/controllers/DogController.java
+++ src/main/java/com/codeup/dogdays/controllers/DogController.java
@@ -1,4 +1,23 @@
 package com.codeup.dogdays.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class DogController {
+
+
+    @GetMapping("/profile/mydogs")
+    public String showDogsProfile() {
+        return "users/mydogs";
+    }
+
+
+    @GetMapping("/users/{id}/mydogs/{dogId}/edit")
+    public String getEditDogUserForm(@PathVariable int id, @PathVariable int dogId) {
+
+        return "users/editDog";
+    }
+
 }
