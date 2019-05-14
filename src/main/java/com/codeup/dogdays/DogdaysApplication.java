bcff9fa7d39427e16cb20e84d3cd87773d448e8d
megansimmons <simmons.megan584@gmail.com>
2019-05-14 09:28:07 -0500

    Initial commit

---
+++ src/main/java/com/codeup/dogdays/DogdaysApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.dogdays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogdaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogdaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/dogdays/DogdaysApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.dogdays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DogdaysApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
