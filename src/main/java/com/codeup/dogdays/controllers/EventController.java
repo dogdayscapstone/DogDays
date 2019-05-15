0249214845c7a3e7328881f0f3597afc590b8400
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-05-15 16:56:44 -0500

    debugged

--- src/main/java/com/codeup/dogdays/controllers/EventController.java
+++ src/main/java/com/codeup/dogdays/controllers/EventController.java
@@ -1,72 +1,86 @@
 package com.codeup.dogdays.controllers;
 
 import com.codeup.dogdays.models.Event;
+import com.codeup.dogdays.models.User;
 import com.codeup.dogdays.repositories.EventRepository;
+import com.codeup.dogdays.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class EventController {
 
     private final EventRepository eventRepo;
+    private final UserRepository userRepo;
 
-    public EventController(EventRepository eventRepo) {
+    public EventController(EventRepository eventRepo, UserRepository userRepo) {
         this.eventRepo = eventRepo;
+        this.userRepo = userRepo;
     }
 
-    @GetMapping("events")
+    @GetMapping("/events")
     public String allEvents(Model model) {
         model.addAttribute("events", eventRepo.findAll());
 
         return "events/events";
     }
 
-    @GetMapping("events/create")
-    public String createPost(Model model) {
+
+
+
+
+
+    @GetMapping("/events/create")
+    public String showPostForm(Model model) {
+
         model.addAttribute("event", new Event());
         return "events/create";
+
     }
 
-    @PostMapping("events/create")
+    @PostMapping("/events/create")
     public String createPost(@ModelAttribute Event event) {
 
-        eventRepo.save(event);
-        return "redirect:events";
+//        User user = (User)request.getSession().getAttribute("user");
+//        User dbUser = userRepo.findOne(user.getId());
+//        event.setUser(dbUser);
 
+        eventRepo.save(event);
+
+        return "redirect:/events" ;
 
     }
 
 
-    @GetMapping("events/{id}")
+
+
+
+
+    @GetMapping("/events/{id}")
     public String getOneBook(Model model, @PathVariable Long id) {
         Event event=eventRepo.findOne(id);
        model.addAttribute("event",event);
         return "events/show";
     }
 
-//    @GetMapping("/posts/{id}/edit")
-//    public String editForm(@PathVariable long id, Model model) {
-//        Post post = postRepo.findOne(id);
-//        model.addAttribute("post", post);
-//        return "posts/edit";
-//    }
 
 
-
-    @GetMapping("events/{id}/edit")
-    public String editForm(@PathVariable int id,Model model) {
+    @GetMapping("/events/{id}/edit")
+    public String editForm(@PathVariable int id, Model model) {
         Event event = eventRepo.findOne((long) id);
         model.addAttribute("event", event);
         return "events/edit";
 
     }
 
-    @PostMapping("events/{id}/edit")
+    @PostMapping("/events/{id}/edit")
     public String editPost(@ModelAttribute Event event) {
         eventRepo.save(event);
-        return "redirect:events/" ;
+        return "redirect:/events" ;
     }
 
 
