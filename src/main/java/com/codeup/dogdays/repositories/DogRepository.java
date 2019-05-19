ab5e6f0b286bd042e74f828cd44a336670746fd7
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-05-18 21:45:50 -0500

    finally done with search by location functionality done

--- src/main/java/com/codeup/dogdays/repositories/DogRepository.java
+++ src/main/java/com/codeup/dogdays/repositories/DogRepository.java
@@ -3,6 +3,14 @@ package com.codeup.dogdays.repositories;
 import com.codeup.dogdays.models.Dog;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DogRepository extends CrudRepository<Dog,Long> {
 
+//    List<Dog> findByBreedlike(String breed);
+
+
+    List<Dog> findByBreedLike(String s);
 }
+
+
--- src/main/java/com/codeup/dogdays/repositories/EventRepository.java
+++ src/main/java/com/codeup/dogdays/repositories/EventRepository.java
@@ -1,10 +1,15 @@
 package com.codeup.dogdays.repositories;
 
 import com.codeup.dogdays.models.Event;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 
     Event findById(long id);
 
+    @Query(nativeQuery = true, value="SELECT * FROM events WHERE location LIKE ?")
+    List<Event> search(String q);
 }
